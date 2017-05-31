5b3532c6001eec7b809e2dee1ae9ccfb4bd479ef
ordnaelmedeiros <lrmarkanjo@gmail.com>
2017-05-30 23:47:03 -0300

    LocalDateTimerAdapter Zoned

--- apidevtools/server/src/main/java/br/com/devtools/apidevtools/core/adapters/LocalDateTimerAdapter.java
+++ apidevtools/server/src/main/java/br/com/devtools/apidevtools/core/adapters/LocalDateTimerAdapter.java
@@ -1,12 +1,19 @@
 package br.com.devtools.apidevtools.core.adapters;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class LocalDateTimerAdapter extends XmlAdapter<String, LocalDateTime> {
     public LocalDateTime unmarshal(String v) throws Exception {
-        return LocalDateTime.parse(v);
+    	LocalDateTime value = null;
+    	try {
+    		value = LocalDateTime.parse(v);
+		} catch (Exception e) {
+			value = ZonedDateTime.parse(v).toLocalDateTime();
+		}
+        return value;
     }
 
     public String marshal(LocalDateTime v) throws Exception {
