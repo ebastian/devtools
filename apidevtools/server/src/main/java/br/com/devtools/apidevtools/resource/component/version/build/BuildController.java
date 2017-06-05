5e3c48d77096e8841be8d62666e3aeece2ee75c6
ordnaelmedeiros <lrmarkanjo@gmail.com>
2017-06-05 20:10:55 -0300

    BuildController upload binary

--- apidevtools/server/src/main/java/br/com/devtools/apidevtools/resource/component/version/build/BuildController.java
+++ apidevtools/server/src/main/java/br/com/devtools/apidevtools/resource/component/version/build/BuildController.java
@@ -124,6 +124,30 @@ public class BuildController extends Controller<Build> {
 		return Response.ok().build();
 	}
 	
+	@POST
+	@Path("{id}/upload")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response upload(byte[] bytes, @PathParam("id") Long buildId) throws RestException {
+		
+		try {
+			
+			Build build = this.get(buildId);
+			
+			Upload upload = new Upload();
+			upload.setBuild(build);
+			upload.setBytes(bytes);
+			
+			this.getSessao().getEm().persist(upload);
+			this.getSessao().commit();
+			
+		} catch (Exception e) {
+			throw new RestException(e);
+		}
+		
+		return Response.ok().build();
+		
+	}
+	
 	@GET
 	@Path("{id}/download")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
@@ -156,82 +180,4 @@ public class BuildController extends Controller<Build> {
 		
 	}
 	
-	
-	/*
-	private Response parts(MultipartFormDataInput multipartFormDataInput) {
-
-		MultivaluedMap<String, String> multivaluedMap = null;
-		String fileName = null;
-		InputStream inputStream = null;
-		String uploadFilePath = null;
-
-		try {
-			Map<String, List<InputPart>> map = multipartFormDataInput.getFormDataMap();
-			List<InputPart> lstInputPart = map.get("uploadedFile");
-
-			for (InputPart inputPart : lstInputPart) {
-
-				// get filename to be uploaded
-				multivaluedMap = inputPart.getHeaders();
-				fileName = getFileName(multivaluedMap);
-
-				if (null != fileName && !"".equalsIgnoreCase(fileName)) {
-
-					// write & upload file to UPLOAD_FILE_SERVER
-					inputStream = inputPart.getBody(InputStream.class, null);
-					uploadFilePath = writeToFileServer(inputStream, fileName);
-
-					// close the stream
-					inputStream.close();
-				}
-			}
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		} finally {
-			// release resources, if any
-		}
-
-		return Response.ok().build();
-
-	}
-	
-	private String writeToFileServer(InputStream inputStream, String fileName) throws IOException {
-		 
-        OutputStream outputStream = null;
-        String qualifiedUploadFilePath = "C:/TesteDownload/"+fileName;
- 
-        try {
-            outputStream = new FileOutputStream(new File(qualifiedUploadFilePath));
-            int read = 0;
-            byte[] bytes = new byte[1024];
-            while ((read = inputStream.read(bytes)) != -1) {
-                outputStream.write(bytes, 0, read);
-            }
-            outputStream.flush();
-        }
-        catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-        finally{
-            //release resource, if any
-            outputStream.close();
-        }
-        return qualifiedUploadFilePath;
-    }
-	
-	private String getFileName(MultivaluedMap<String, String> multivaluedMap) {
-		 
-        String[] contentDisposition = multivaluedMap.getFirst("Content-Disposition").split(";");
- 
-        for (String filename : contentDisposition) {
- 
-            if ((filename.trim().startsWith("filename"))) {
-                String[] name = filename.split("=");
-                String exactFileName = name[1].trim().replaceAll("\"", "");
-                return exactFileName;
-            }
-        }
-        return "UnknownFile";
-    }
-	*/
 }
