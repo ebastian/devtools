00bd8168bbe7a9c8a44359a8bde076b9849197de
Eduardo <eduardo.bastian@gmail.com>
2017-06-17 17:45:12 -0300

    Fix ProductComponent mock

--- webdevtools/src/app/products-components/service/products-components.mock.ts
+++ webdevtools/src/app/products-components/service/products-components.mock.ts
@@ -4,36 +4,42 @@ export const PRODUCTS_COMPONENTS: Array<ProductComponent> = [{
   'id': 1,
   'name': 'Base de Dados Saúde',
   'description': 'Base de informações dos sistemas Saúde.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': null
 }, {
   'id': 2,
   'name': 'WinSaúde',
   'description': 'Sistema Saúde para desktop.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': null
 }, {
   'id': 3,
   'name': 'IDSSaúde',
   'description': 'Sistema Saúde para Web.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': null
 }, {
   'id': 4,
   'name': 'IDS Esf',
   'description': 'Aplicativo para Estratégia de Saúde da Família. Controle de Visitas domiciliares via tablet.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': null
 }, {
   'id': 5,
   'name': 'IDSSaudeWS',
   'description': 'API Rest com métodos para: App IDS Esf, App IDS Endemias, Integração de Agendamento de Consultas, Integração de Estoque com Farmácias Particulares.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': null
 }, {
   'id': 6,
   'name': 'NetSaúde',
   'description': 'Portal de Agendamento de Consultas WEB.',
+  'fileName': 'arquivo.exe',
   'creation': new Date(),
   'death': new Date()
 }]
@@ -42,66 +48,66 @@ export const PRODUCTS_COMPONENTS: Array<ProductComponent> = [{
   'id': 5,
   'name': 'Jodie',
   'description': 'Simpole',
-  'creation': 'jsimpole4@abc.net.au',
+  'fileName': 'arquivo.exe',\n\t'creation': 'jsimpole4@abc.net.au',
   'death': null
 }, {
   'id': 6,
   'name': 'Cherey',
   'description': 'Goodge',
-  'creation': 'cgoodge5@51.la',
+  'fileName': 'arquivo.exe',\n\t'creation': 'cgoodge5@51.la',
   'death': 'Female'
 }, {
   'id': 7,
   'name': 'Reinaldo',
   'description': 'Gauler',
-  'creation': 'rgauler6@buzzfeed.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'rgauler6@buzzfeed.com',
   'death': 'Male'
 }, {
   'id': 8,
   'name': 'Torr',
   'description': 'Mitroshinov',
-  'creation': 'tmitroshinov7@topsy.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'tmitroshinov7@topsy.com',
   'death': 'Male'
 }, {
   'id': 9,
   'name': 'Lexie',
   'description': 'Nilles',
-  'creation': 'lnilles8@discuz.net',
+  'fileName': 'arquivo.exe',\n\t'creation': 'lnilles8@discuz.net',
   'death': 'Female'
 }, {
   'id': 10,
   'name': 'Normy',
   'description': 'Hold',
-  'creation': 'nhold9@earthlink.net',
+  'fileName': 'arquivo.exe',\n\t'creation': 'nhold9@earthlink.net',
   'death': 'Male'
 }, {
   'id': 11,
   'name': 'Judy',
   'description': 'Leaney',
-  'creation': 'jleaneya@last.fm',
+  'fileName': 'arquivo.exe',\n\t'creation': 'jleaneya@last.fm',
   'death': null
 }, {
   'id': 12,
   'name': 'Eduard',
   'description': 'Lumsdon',
-  'creation': 'elumsdonb@zdnet.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'elumsdonb@zdnet.com',
   'death': 'Male'
 }, {
   'id': 13,
   'name': 'Zorine',
   'description': 'Clementi',
-  'creation': 'zclementic@blogspot.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'zclementic@blogspot.com',
   'death': 'Female'
 }, {
   'id': 14,
   'name': 'Sigismundo',
   'description': 'Marjoram',
-  'creation': 'smarjoramd@wunderground.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'smarjoramd@wunderground.com',
   'death': 'Male'
 }, {
   'id': 15,
   'name': 'Felecia',
   'description': 'Heathcott',
-  'creation': 'fheathcotte@geocities.com',
+  'fileName': 'arquivo.exe',\n\t'creation': 'fheathcotte@geocities.com',
   'death': 'Female'
 }];*/
